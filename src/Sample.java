59314802c44f5011589b6a89e2b9669ceca87f41
James Morris <jm20@sanger.ac.uk>
2011-08-16 17:06:02 +0000

    moved the code to compare chromosomes into the SampleCompare class.

--- src/Sample.java
+++ src/Sample.java
@@ -1,61 +1,26 @@
-import java.util.*;
+
 
 public class Sample {
 
-	//array of haplotypes - one for each autosome
-	public Haplotype[] matHaps;
-	public Haplotype[] patHaps;
+	// for the moment just work with a single chromosome
+	public Haplotype matHap;
+	public Haplotype patHap;
 	
-	public Sample() {
-		
+	public Sample(Haplotype m, Haplotype p) {
+		matHap = m;
+		patHap = p;
 	}
-	
-	
-	//Jeff's code
-	public Vector<Segment> compareChromosomes(Haplotype a, Haplotype b){
-		Vector<Segment> matches = new Vector();
-		
-		Enumeration<Segment> hapASegs = a.getSegments();
-		Enumeration<Segment> hapBSegs = b.getSegments();
 
-		Segment currentSegA;
-		Segment currentSegB;
-		while (hapASegs.hasMoreElements() || hapBSegs.hasMoreElements()){
-			
-			if (currentSegA.equals(null)){
-				//This is the first entrance into the loop, so just pull out the first seg from each and continue
-				currentSegA = hapASegs.nextElement();
-				currentSegB = hapBSegs.nextElement();
-				continue;
-			}
-			
-			if (currentSegA.getEnd() > currentSegB.getEnd()){
-				currentSegB = hapBSegs.nextElement();
-			}else{
-				currentSegA = hapASegs.nextElement();
-			}
-			
-			if ((currentSegA.getStart() >= currentSegB.getStart() && currentSegA.getStart() <= currentSegB.getEnd()) ||
-					(currentSegA.getEnd() >= currentSegB.getStart() && currentSegA.getEnd() <= currentSegB.getEnd()) ||
-					(currentSegA.getStart() >= currentSegB.getStart() && currentSegA.getEnd() >= currentSegB.getEnd())){
-				if (currentSegA.getCode() == currentSegB.getCode()){
-					int start;
-					if (currentSegA.getStart() > currentSegB.getStart()){
-						start = currentSegA.getStart();
-					}else{
-						start = currentSegB.getStart();
-					}
-					
-					int end;
-					if (currentSegA.getEnd() < currentSegB.getEnd()){
-						end = currentSegA.getEnd();
-					}else{
-						end = currentSegB.getEnd();
-					}
-					
-					matches.add(new Segment(start,end,currentSegA.getCode())); 
-				}
-			}
-		}
+	public Sample() {
+
 	}
+
+	public Haplotype getMat() {
+		return matHap;
+	}
+
+	public Haplotype getPat() {
+		return patHap;
+	}
+	
 }
