ea2a13be21b5e716eb3cb0ca58c33ccb24dcbe27
James Morris <jm20@sanger.ac.uk>
2011-09-19 14:21:36 +0000

    changed the class to expect tabix files

--- src/FreqFile.java
+++ src/FreqFile.java
@@ -1,30 +1,31 @@
 import java.io.BufferedReader;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
+import java.io.File;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.zip.GZIPInputStream;
 
 
 public class FreqFile {
 
-	BufferedReader ffr;
-	
-	public FreqFile(String fileName) throws FileNotFoundException, IOException {
-		if (fileName.endsWith(".gz")) {
-			ffr = new BufferedReader(new InputStreamReader(new GZIPInputStream(new FileInputStream(fileName))));
+        TabixReader tabixFreq;
+    	
+	public FreqFile(String fileName) throws Exception {
+		File freqIndex = new File(fileName + ".tbi");
+                
+                System.out.println(fileName);
+                System.out.println(freqIndex.getAbsoluteFile());
+                
+		if (fileName.endsWith("gz")) {
+			if (freqIndex.exists()) {
+				tabixFreq = new TabixReader(fileName);
+			} else {
+				throw new Exception("Freq file needs to be indexed using tabix");
+			}
 		} else {
-			ffr = new BufferedReader(new InputStreamReader(new FileInputStream(fileName)));
+			throw new Exception("Freq file needs to be compressed using bgzip");
 		}
 	}
 	
-	public BufferedReader getReader() {	
-		return ffr;
-	}
-	
-	public String readLine() throws IOException {
-		// method for testing
-		return ffr.readLine();
+	public TabixReader getReader() {	
+		return tabixFreq;
 	}
 	
 }
