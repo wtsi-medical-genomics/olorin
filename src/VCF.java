9a9b7c48fdffea15cb1591e8adcaf2be39d4f53a
James Morris <jm20@sanger.ac.uk>
2011-09-29 17:11:12 +0000

    filtering on frequency now working

--- src/VCF.java
+++ src/VCF.java
@@ -1,117 +1,107 @@
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Enumeration;
-import java.util.Hashtable;
-import java.util.Vector;
-import java.util.zip.GZIPInputStream;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class VCF {
+    
+    TabixReader tabixVCF;
+    VCFMeta meta;
+    
+    public VCF(final String fileName) throws Exception {
+        File vcfIndex = new File(fileName + ".tbi");
+        if (fileName.endsWith("gz")) {
+            if (vcfIndex.exists()) {
+                tabixVCF = new TabixReader(fileName);
+                this.parseHeader();
+            } else {
+                throw new Exception("VCF needs to be indexed using tabix");
+            }
+        } else {
+            throw new Exception("VCF needs to be compressed using bgzip");
+        }
+    }
+    
+    private void parseHeader() throws IOException {
+        meta = new VCFMeta();
+        String line = tabixVCF.readLine();
+        while (line.startsWith("#")) {
+            meta.add(line);
+            line = tabixVCF.readLine();
+        }
+        // the minimum meta info for a vcf header is the fileformat and the the header line
+        // die "Not a valid VCF" unless the input vcf has at least these two lines 
+    }
+    
+    public ArrayList<Variant> getVariants(ArrayList<SegmentMatch> matches, int matchNum) throws IOException {
+        
+        ArrayList<Variant> variants = new ArrayList<Variant>();
+        
+        for (SegmentMatch m : matches) {
+            
+            if (m.getIds().size() >= matchNum) {
+                if (tabixVCF.query(m.getChr() + ":" + m.getStart() + "-" + m.getEnd()) != null) {
+                    TabixReader.Iterator i = tabixVCF.query(m.getChr() + ":" + m.getStart() + "-" + m.getEnd());
+                    if (i.next() != null) {
+                        String vcfLine = i.next();
+                        while (vcfLine != null) {
+                            variants.add(new Variant(vcfLine));
+                            vcfLine = i.next();
+                        }
+                    }
+                }
+            }
+        }
+        return variants;
+    }
 
-	TabixReader tabixVCF;
-	VCFMeta meta;
-	
-	public VCF(final String fileName) throws Exception {
-		File vcfIndex = new File(fileName + ".tbi");
-		if (fileName.endsWith("gz")) {
-			if (vcfIndex.exists()) {
-				tabixVCF = new TabixReader(fileName);
-				this.parseHeader();
-			} else {
-				throw new Exception("VCF needs to be indexed using tabix");
-			}
-		} else {
-			throw new Exception("VCF needs to be compressed using bgzip");
-		}
-	}
-		
-	private void parseHeader() throws IOException {
-		meta = new VCFMeta();
-		String line = tabixVCF.readLine();
-		while(line.startsWith("#")) {
-			meta.add(line);
-			line = tabixVCF.readLine();
-		}		
-		// the minimum meta info for a vcf header is the fileformat and the the header line
-		// die "Not a valid VCF" unless the input vcf has at least these two lines 
-	}
-	
-	
-	public ArrayList<Variant> getVariants(ArrayList<SegmentMatch> matches, int matchNum) throws IOException {
-		
-		ArrayList<Variant> variants = new ArrayList<Variant> ();
-		
-		for(SegmentMatch m : matches) {
-			
-			if (m.getIds().size() >= matchNum) {
-				if (tabixVCF.query(m.getChr() + ":" + m.getStart() + "-" + m.getEnd()) != null) {
-					TabixReader.Iterator i = tabixVCF.query(m.getChr() + ":" + m.getStart() + "-" + m.getEnd());
-					if (i.next() != null) {
-						String vcfLine = i.next();
-						while (vcfLine != null) {
-							variants.add(new Variant(vcfLine));
-							vcfLine = i.next();
-						}
-					}
-				}
-			}
-		}
-		return variants;
-	}
-
-	public ArrayList<Variant> getVariants(ArrayList<SegmentMatch> matches, int matchNum, FreqFile ff, double cutoff) throws NumberFormatException, IOException {
-//		TabixReader tabixFreq = ff.getReader();
-		ArrayList<Variant> variants = new ArrayList<Variant> ();
-//		for(SegmentMatch m : matches) {
-//			if (m.getIds().size() >= matchNum) {
-//				// get all the frequency values for this region
-//				// store them in a hash on position
-//				Hashtable freqs = new Hashtable ();
-//				TabixReader.Iterator f = tabixFreq.query(m.getChr() + ":" + m.getStart() + "-" + m.getEnd());
-//				String freqLine = f.next();
-//				while (freqLine != null) {
-//					String[] vals = freqLine.split("\\s+");
-//					String pos = vals[1];
-//					int nAlleles = Integer.parseInt(vals[2]);
-//					double mFreq;
-//					for (int i = 4; i < nAlleles + 4; i++ ) {
-//						Double freq = Double.parseDouble(vals[i]);
-//						if (freq == null) {
-//							mFreq = freq;
-//						} else if  (freq < mFreq) {
-//							mFreq = freq;
-//						}
-//					}
-//					freqs.put(pos, mFreq);	
-//					freqLine = f.next();
-//				}
-//				
-//				TabixReader.Iterator i = tabixVCF.query(m.getChr() + ":" + m.getStart() + "-" + m.getEnd());
-//				String vcfLine = i.next();
-//				while (vcfLine != null) {
-//					Variant var = new Variant(vcfLine);
-//					if (freqs.containsKey(var.getPos())) {
-//						if (freqs.get(var.getPos()) < cutoff) {
-//							variants.add(var);
-//						}
-//					} else {
-//						// variant not in freq file - so include variant
-//						variants.add(var);
-//					}
-//					vcfLine = i.next();
-//				}
-//			}
-//		}
-		return variants;
-	}
-	
-	public VCFMeta getMeta() {
-		return meta;
-	}
-	
+    public ArrayList<Variant> getVariants(ArrayList<SegmentMatch> matches, int matchNum, String ff, double cutoff) throws NumberFormatException, IOException {
+        
+        TabixReader tabixFreq = new TabixReader(ff);
+        ArrayList<Variant> variants = new ArrayList<Variant>();
+        for (SegmentMatch m : matches) {
+            if (m.getIds().size() >= matchNum) {
+                if (tabixVCF.query(m.getChr() + ":" + m.getStart() + "-" + m.getEnd()) != null) {
+                    TabixReader.Iterator vcfi = tabixVCF.query(m.getChr() + ":" + m.getStart() + "-" + m.getEnd());
+                    String vcfLine = vcfi.next();
+                    while (vcfLine != null) {
+                        Variant var = new Variant(vcfLine);
+                        // for each variant check if it exists in the freq file
+                        // if it does is it below the frequency cutoff?
+                        if (tabixFreq.query(m.getChr() + ":" + var.getPos() + "-" + var.getPos()) != null) {
+                            TabixReader.Iterator f = tabixFreq.query(m.getChr() + ":" + var.getPos() + "-" + var.getPos());
+                            String freqLine = f.next();
+                            if (freqLine != null) {
+                                String[] vals = freqLine.split("\\t+");
+                                ArrayList<Double> freqVals = new ArrayList<Double>();
+                                int nAlleles = Integer.parseInt(vals[2]);
+                                for (int i = 4; i < nAlleles + 4; i++) {
+                                    String[] frequency = vals[i].split(":");
+                                    Double freqVal = Double.parseDouble(frequency[1]);
+                                    freqVals.add(freqVal);
+                                }
+                                Double minFreq = Collections.min(freqVals);
+                                if (minFreq < cutoff) {
+                                    var.setFreq(minFreq);
+                                    variants.add(var);
+                                }      
+                            }
+                            // what if there is more than 1 line?
+                        } else {
+                            // variant not in freq file - so include
+                            var.setFreq(0.0);
+                            variants.add(var);
+                        }
+                        vcfLine = vcfi.next();
+                    }
+                }
+            }
+        }
+        return variants;
+    }
+    
+    public VCFMeta getMeta() {
+        return meta;
+    }
 }
