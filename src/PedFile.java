177659cef591ddaa6284ad4a8f13c0526b6f9080
James Morris <jm20@sanger.ac.uk>
2011-10-05 16:52:22 +0000

    return a list of all the samples in the ped, for comparing against the samples in the vcf

--- src/PedFile.java
+++ src/PedFile.java
@@ -6,21 +6,24 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintStream;
+import java.util.ArrayList;
 
 // Class to reformat the input ped format file for use with the pedViz library
 
 public class PedFile {
 	
 	String fileName;
+        ArrayList<String> samples;
 	
 	public PedFile(String fn) throws Exception {
 		fileName = fn;	
+                samples = new ArrayList();
 	}
 	
 	public String makeCSV() throws IOException {
 		String csvFileName = fileName + ".csv";
 		PrintStream output = new PrintStream(new FileOutputStream(new File(csvFileName)));
-		output.println("Id,Fid,Mid,Sex,aff");
+		output.println("Id,Fid,Mid,Sex,aff,seq");
 		BufferedReader pfr = new BufferedReader(new InputStreamReader(new FileInputStream(fileName)));
 		String line;
 		while((line=pfr.readLine()) != null) {
@@ -30,9 +33,15 @@ public class PedFile {
 			String matID = values[3];
 			String sex   = values[4];
 			String aff   = values[5];
-			output.println(ID +","+ patID +","+ matID +","+ sex +","+ aff);
+			output.println(ID +","+ patID +","+ matID +","+ sex +","+ aff + ",1");
+                        samples.add(ID);
 		}
 		output.close();
 		return csvFileName;
 	}
+        
+        public ArrayList<String> getSamples() {
+            return samples;
+        }
+        
 }
