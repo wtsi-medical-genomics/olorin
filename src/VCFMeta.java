86993f8364e36232d47f2475254e4abf937761ea
James Morris <jm20@sanger.ac.uk>
2011-10-05 16:53:24 +0000

    changed using Hashtable for the more up to date HashMap

--- src/VCFMeta.java
+++ src/VCFMeta.java
@@ -1,23 +1,23 @@
 import java.util.ArrayList;
-import java.util.Hashtable;
+import java.util.HashMap;
 
 
 public class VCFMeta {
 	
-	ArrayList<Hashtable<String, String>> info;
-	ArrayList<Hashtable<String, String>> filter;
-	ArrayList<Hashtable<String, String>> format;
-	Hashtable<String,String> other;
+	ArrayList<HashMap<String, String>> info;
+	ArrayList<HashMap<String, String>> filter;
+	ArrayList<HashMap<String, String>> format;
+	HashMap<String,String> other;
 	ArrayList<String> samples;
 	ArrayList<String> infoIds;
 	
 	// replace the arraylists with info, filter and format objects?
 	
 	public VCFMeta() {
-		info   = new ArrayList<Hashtable<String,String>> ();
-		filter = new ArrayList<Hashtable<String,String>> ();
-		format = new ArrayList<Hashtable<String,String>> ();
-		other  = new Hashtable<String,String> ();
+		info   = new ArrayList<HashMap<String,String>> ();
+		filter = new ArrayList<HashMap<String,String>> ();
+		format = new ArrayList<HashMap<String,String>> ();
+		other  = new HashMap<String,String> ();
 		infoIds = new ArrayList<String> ();
 	}
 	
@@ -29,7 +29,7 @@ public class VCFMeta {
 				//split on the comma only if that comma has zero, or an even number of quotes in ahead of it
 				// for explaination see here http://stackoverflow.com/questions/1757065/java-splitting-a-comma-separated-string-but-ignoring-commas-in-quotes
 				String[] values2 = values[1].split(",(?=([^\"]*\"[^\"]*\")*[^\"]*$)");
-				Hashtable ht = new Hashtable<String,String> ();
+				HashMap ht = new HashMap<String,String> ();
 				for (String val : values2) {
 					if (val.contains("=")) {
 						String[] values3 = val.split("=");
@@ -45,7 +45,7 @@ public class VCFMeta {
 			} else if (s.startsWith("FILTER")) {
 				String[] values = s.trim().split("[<>]");
 				String[] values2 = values[1].split(",(?=([^\"]*\"[^\"]*\")*[^\"]*$)");
-				Hashtable ht = new Hashtable<String,String> ();
+				HashMap ht = new HashMap<String,String> ();
 				for (String val : values2) {
 					String[] values3 = val.split("=");
 					ht.put(values3[0], values3[1]);
@@ -54,7 +54,7 @@ public class VCFMeta {
 			} else if (s.startsWith("FORMAT")) {
 				String[] values = s.trim().split("[<>]");
 				String[] values2 = values[1].split(",(?=([^\"]*\"[^\"]*\")*[^\"]*$)");
-				Hashtable ht = new Hashtable<String,String> ();
+				HashMap ht = new HashMap<String,String> ();
 				for (String val : values2) {
 					String[] values3 = val.split("=");
 					ht.put(values3[0], values3[1]);
@@ -82,15 +82,15 @@ public class VCFMeta {
 		return other.get("fileformat");
 	}
 
-	public ArrayList<Hashtable<String,String>> getInfo() {
+	public ArrayList<HashMap<String,String>> getInfo() {
 		return info;
 	}
 
-	public ArrayList<Hashtable<String,String>> getFilter() {
+	public ArrayList<HashMap<String,String>> getFilter() {
 		return filter;
 	}
 
-	public ArrayList<Hashtable<String,String>> getFormat() {
+	public ArrayList<HashMap<String,String>> getFormat() {
 		return format;
 	}
 
