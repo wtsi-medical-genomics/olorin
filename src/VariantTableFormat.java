bf59ab42a442282130e605863e832ff650b3e3dc
James Morris <jm20@sanger.ac.uk>
2012-01-13 15:06:11 +0000

    User selected CSQ columns.

--- src/VariantTableFormat.java
+++ src/VariantTableFormat.java
@@ -4,12 +4,10 @@ import java.util.ArrayList;
 
 class VariantTableFormat implements TableFormat {
 
-    private ArrayList<String> usrSelectedCols;
     private ArrayList<String> indIds;
     public ArrayList<String> cols;
 
     VariantTableFormat(ArrayList<String> selectedCols, Boolean freqFilter, ArrayList<String> indIds) {
-        usrSelectedCols = selectedCols;
         cols = new ArrayList();
         cols.add("Chromosome");
         cols.add("Position");
@@ -23,29 +21,12 @@ class VariantTableFormat implements TableFormat {
             cols.add("Frequency");
         }
         cols.addAll(indIds);
-        Boolean csqSelected = false;
-        for (String s : usrSelectedCols) {
-            if (s.matches("CSQ")) {
-                csqSelected = true;
-            } else {
-                cols.add(s);
-            }
-        }
 
-        if (csqSelected) {            
-            cols.add("CSQ-Gene");
-            cols.add("CSQ-Consequence");
-            cols.add("CSQ-Amino acid change");
-            cols.add("CSQ-Condel prediction");
-            cols.add("CSQ-Condel score");
-            cols.add("CSQ-SIFT prediction");
-            cols.add("CSQ-SIFT score");
-            cols.add("CSQ-PloyPhen prediction");
-            cols.add("CSQ-PolyPhen score");
-            cols.add("CSQ-Grantham score");
-            cols.add("CSQ-GERP score");
-            cols.add("Total effects");
+        for (String s : selectedCols) {
+            cols.add(s);
         }
+    
+        cols.add("Total effects");
     }
 
     @Override
