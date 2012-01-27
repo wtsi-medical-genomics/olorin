236dca8cf0a6a442c348b8b4beb0b42dff264f47
James Morris <jm20@sanger.ac.uk>
2012-01-27 15:17:20 +0000

    Added a catch for badly formatted position values.

--- src/MapFile.java
+++ src/MapFile.java
@@ -1,38 +1,44 @@
+
 import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.zip.GZIPInputStream;
-
+import javax.swing.JOptionPane;
 
 public class MapFile {
 
-	BufferedReader mfr;
-	ArrayList<Integer> positions;
-	
-	public MapFile (String fileName) throws IOException {
-		if (fileName.endsWith(".gz")) {
-			mfr = new BufferedReader(new InputStreamReader(new GZIPInputStream(new FileInputStream(fileName))));
-		} else {
-			mfr = new BufferedReader(new InputStreamReader(new FileInputStream(fileName)));
-		}
-		parseMap();
-	}
+    String chrom;
+    BufferedReader mfr;
+    ArrayList<Integer> positions;
 
-	private void parseMap() throws IOException {
-		String line;
-		positions = new ArrayList<Integer> ();		
-		while((line=mfr.readLine()) != null) {
-			if (!line.isEmpty()) {
-				String[] values = line.trim().split("\\s+");
-				positions.add(new Integer(Integer.parseInt(values[3])));
-			}
-		}
-	}
+    public MapFile(String fileName, String chrom) throws IOException {
+        this.chrom = chrom;
+        if (fileName.endsWith(".gz")) {
+            mfr = new BufferedReader(new InputStreamReader(new GZIPInputStream(new FileInputStream(fileName))));
+        } else {
+            mfr = new BufferedReader(new InputStreamReader(new FileInputStream(fileName)));
+        }
+        parseMap();
+    }
 
-	public ArrayList<Integer> getPositions() {
-		return positions;
-	}
+    private void parseMap() throws IOException {
+        String line;
+        positions = new ArrayList<Integer>();
+        while ((line = mfr.readLine()) != null) {
+            if (!line.isEmpty()) {
+                String[] values = line.trim().split("\\s+");
+                try {
+                    positions.add(new Integer(Integer.parseInt(values[3])));
+                } catch (NumberFormatException nfe) {
+                    JOptionPane.showMessageDialog(null, "Unrecognised position format " + values[3] + " in chromosome " + chrom, "Error", JOptionPane.ERROR_MESSAGE);
+                }
+            }
+        }
+    }
 
+    public ArrayList<Integer> getPositions() {
+        return positions;
+    }
 }
