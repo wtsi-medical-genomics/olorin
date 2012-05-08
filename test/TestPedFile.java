a7cc4b73e68a23372ceaf06b0cda89a5e010f028
James Morris <jm20@sanger.ac.uk>
2012-05-08 17:01:00 +0000

    removed useless tests

--- test/TestPedFile.java
+++ test/TestPedFile.java
@@ -12,27 +12,12 @@ import junit.framework.TestCase;
 public class TestPedFile extends TestCase {
 
 	@Test public void testParsePed() throws Exception {
-		PedFile ped = new PedFile("test_files/test.22.ped");
+		PedFile ped = new PedFile("/Users/jm20/work/workspace/Oberon_svn/test/test_files/test.ped");
 		
 		String csvFile = ped.makeCSV();
-		assertEquals("test_files/test.22.ped.csv", csvFile);
+		assertEquals("/Users/jm20/work/workspace/Oberon_svn/test/test_files/test.ped.csv", csvFile);
 		
-		Graph graph = new Graph();
-		CsvGraphLoader loader = new CsvGraphLoader(csvFile, ",");
-		loader.setSettings("Id", "Mid", "Fid");
-		loader.load(graph);
-
-		//Step 2
-		Sugiyama s = new Sugiyama(graph);
-		s.run();
-
-		//Creates a frame
-		JFrame frame = new JFrame();
-		frame.setSize(800, 600);
-		
-		//Step 3
-		GraphView2D view = new GraphView2D(s.getLayoutedGraph());		
-		frame.add(view.getComponent());
-		frame.setVisible(true);
+		// read the csv file and make sure it is what we expect
+                
 	}	
 }
