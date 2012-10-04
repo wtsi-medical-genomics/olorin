41bc448b4aee1f8c32be91847e89e36054a38d8b
James Morris <jm20@sanger.ac.uk>
2012-10-04 15:02:25 +0000

    changes made to allow loading single whole genome map and flow files
    Former-commit-id: 0481770f28399d719b33d8c8fbb85cdd29b93fe5

--- src/FlowFile.java
+++ src/FlowFile.java
@@ -14,11 +14,24 @@ public class FlowFile {
     String chrom;
     BufferedReader ffr;
     HashMap<String, Chromosome> individuals;
+    HashMap<String, Sample> samples;
     ArrayList<Integer> positions;
+    private HashMap<String, ArrayList<Integer>> positionsByChromosome;
+    boolean singleFile;
 
     public FlowFile(String fileName, String chrom) throws Exception {
         this.filename = filename;
         this.chrom = chrom;
+        if (fileName.endsWith(".gz")) {
+            ffr = new BufferedReader(new InputStreamReader(new GZIPInputStream(new FileInputStream(fileName))));
+        } else {
+            ffr = new BufferedReader(new InputStreamReader(new FileInputStream(fileName)));
+        }
+    }
+
+    public FlowFile(String fileName) throws IOException {
+        singleFile = true;
+        this.filename = filename;
         if (fileName.endsWith(".gz")) {
             ffr = new BufferedReader(new InputStreamReader(new GZIPInputStream(new FileInputStream(fileName))));
         } else {
@@ -33,37 +46,108 @@ public class FlowFile {
         ArrayList<Segment> patHap = null;
 
         try {
-            while ((line = ffr.readLine()) != null) {
-                if (!line.isEmpty()) {
-                    //assume 1 family per file
-                    if (!line.startsWith("FAMILY")) {
+            if (singleFile) {
+                // assume chromosomes are numericaly sorted
+                Integer flowChr = 0;
+                samples = new HashMap<String, Sample>();
 
-                        String[] codes = line.trim().split("\\s+");                                                
-                        // subtract 2 from the flow file length to account for the id and status                        
-                        if ((codes.length-2) != positions.size()) {
-                            JOptionPane.showMessageDialog(null, "Number of markers in .map and .flow files does not match in chromosome " + chrom, "Error", JOptionPane.ERROR_MESSAGE);
-                            return;                            
-                        }
-
-                        String id = codes[0];
-                        String status = codes[1];
-                        ArrayList<Segment> seg = findSegments(codes);
-                        if (status.contains("FOUNDER")) {
-                            if (matHap != null) {
-                                patHap = seg;
-                            } else {
-                                matHap = seg;
+                while ((line = ffr.readLine()) != null) {
+                    if (!line.isEmpty()) {
+                        //assume 1 family per file
+                        if (!line.startsWith("FAMILY")) {
+                            String[] codes = line.trim().split("\\s+");
+                            // subtract 2 from the flow file length to account for the id and status                            
+                            if ((codes.length - 2) != positionsByChromosome.get(flowChr.toString()).size()) {
+                                JOptionPane.showMessageDialog(null, "Number of markers in .map and .flow files does not match in chromosome " + chrom, "Error", JOptionPane.ERROR_MESSAGE);
+                                return;
                             }
-                        } else if (status.contains("MATERNAL")) {
-                            matHap = seg;
-                        } else if (status.contains("PATERNAL")) {
-                            patHap = seg;
-                        }
+                            String id = codes[0];
+                            String status = codes[1];
+                            ArrayList<Segment> seg = findSegments(codes, flowChr);
+                            if (status.contains("FOUNDER")) {
+                                if (matHap != null) {
+                                    patHap = seg;
+                                } else {
+                                    matHap = seg;
+                                }
+                            } else if (status.contains("MATERNAL")) {
+                                matHap = seg;
+                            } else if (status.contains("PATERNAL")) {
+                                patHap = seg;
+                            }
 
-                        if (matHap != null && patHap != null) {
-                            individuals.put(id, new Chromosome(matHap, patHap));
-                            matHap = null;
-                            patHap = null;
+                            if (matHap != null && patHap != null) {
+                                individuals.put(id, new Chromosome(matHap, patHap));
+                                matHap = null;
+                                patHap = null;
+                            }
+
+                        } else if (!individuals.isEmpty()) {
+                            for (String indID : individuals.keySet()) {
+                                if (samples.containsKey(indID)) {
+                                    Sample samp = samples.get(indID);
+                                    samp.setChr(flowChr.toString(), individuals.get(indID));
+                                } else {
+                                    Sample samp = new Sample();
+                                    samp.setChr(flowChr.toString(), individuals.get(indID));
+                                    samples.put(indID, samp);
+                                }
+                            }
+                            individuals.clear();
+                            flowChr++;
+                        } else {
+                            // skip first chromosome - as no segments have been recorded yet
+                            flowChr++;
+                        }
+                    }
+                }
+
+                if (!individuals.isEmpty()) {
+                    for (String indID : individuals.keySet()) {
+                        if (samples.containsKey(indID)) {
+                            Sample samp = samples.get(indID);
+                            samp.setChr(flowChr.toString(), individuals.get(indID));
+                        } else {
+                            Sample samp = new Sample();
+                            samp.setChr(flowChr.toString(), individuals.get(indID));
+                            samples.put(indID, samp);
+                        }
+                    }
+                }
+
+            } else {
+                while ((line = ffr.readLine()) != null) {
+                    if (!line.isEmpty()) {
+                        //assume 1 family per file
+                        if (!line.startsWith("FAMILY")) {
+
+                            String[] codes = line.trim().split("\\s+");
+                            // subtract 2 from the flow file length to account for the id and status
+                            if ((codes.length - 2) != positions.size()) {
+                                JOptionPane.showMessageDialog(null, "Number of markers in .map and .flow files does not match in chromosome " + chrom, "Error", JOptionPane.ERROR_MESSAGE);
+                                return;
+                            }
+
+                            String id = codes[0];
+                            String status = codes[1];
+                            ArrayList<Segment> seg = findSegments(codes);
+                            if (status.contains("FOUNDER")) {
+                                if (matHap != null) {
+                                    patHap = seg;
+                                } else {
+                                    matHap = seg;
+                                }
+                            } else if (status.contains("MATERNAL")) {
+                                matHap = seg;
+                            } else if (status.contains("PATERNAL")) {
+                                patHap = seg;
+                            }
+
+                            if (matHap != null && patHap != null) {
+                                individuals.put(id, new Chromosome(matHap, patHap));
+                                matHap = null;
+                                patHap = null;
+                            }
                         }
                     }
                 }
@@ -98,16 +182,54 @@ public class FlowFile {
         return segs;
     }
 
+    private ArrayList<Segment> findSegments(String[] codes, Integer flowChr) {
+        ArrayList<Segment> segs = new ArrayList<Segment>();
+        String id = codes[0];
+        String last = codes[2];
+        int segStart = getPos(2, flowChr);
+        int segEnd = getPos(2, flowChr);
+
+        for (int i = 3; i < codes.length; i++) {
+            String code = codes[i];
+            if (code.contentEquals(last)) {
+                segEnd = getPos(i, flowChr);
+            } else {
+                // store the segment
+                segs.add(new Segment(segStart, segEnd, last.getBytes(), id));
+                //reset the values for the next segment
+                segStart = getPos(i, flowChr);
+                segEnd = getPos(i, flowChr);
+            }
+            last = code;
+        }
+        //last segment
+        segs.add(new Segment(segStart, segEnd, last.getBytes(), id));
+        return segs;
+    }
+
     private int getPos(int i) {
         //offset for the two extra columns in the flow file id and status
         return positions.get(i - 2);
+    }
+
+    private int getPos(int i, Integer flowChr) {
+        return positionsByChromosome.get(flowChr.toString()).get(i - 2);
     }
 
     public HashMap<String, Chromosome> getSamples() {
         return individuals;
     }
 
-    public void setPos(ArrayList<Integer> vector) {
-        positions = vector;
+    public HashMap<String, Sample> getSamplesAllChromosomes() {
+        return samples;
     }
+
+    public void setPos(ArrayList<Integer> positions) {
+        this.positions = positions;
+    }
+
+    public void setPos(HashMap<String, ArrayList<Integer>> positionsByChromosome) {
+        this.positionsByChromosome = positionsByChromosome;
+    }
+
 }
