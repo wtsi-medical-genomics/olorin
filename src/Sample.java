79596a7245aea5dc248446b64a0659eb65819c92
James Morris <jm20@sanger.ac.uk>
2012-10-04 15:03:10 +0000

    changes made to use HashMap not the old Hashtable
    Former-commit-id: ec830b7ab01211f0336085b4042e6756e38c8ebc

--- src/Sample.java
+++ src/Sample.java
@@ -1,37 +1,33 @@
 import java.util.ArrayList;
 import java.util.Enumeration;
-import java.util.Hashtable;
+import java.util.HashMap;
 import java.util.Iterator;
+import java.util.Set;
 import java.util.Vector;
 
 
 public class Sample {
 
-	public Hashtable<String, Chromosome> chr = new Hashtable<String, Chromosome> ();
-	public String id;
+	public HashMap<String, Chromosome> chr = new HashMap<String, Chromosome> ();
 	
-	public Sample(String c, Chromosome s) {
-		chr.put(c, s);
+	public Sample(String chrNum, Chromosome chrObj) {
+		chr.put(chrNum, chrObj);
 	}
 
 	public Sample() {
 
 	}
 
-	public Chromosome getChr (String c) { 
-		return chr.get(c);
+	public Chromosome getChr (String chrNum) { 
+		return chr.get(chrNum);
 	}
 	
-	public void setChr (String c, Chromosome s) {
-		chr.put(c, s);
+	public void setChr (String chrNum, Chromosome chrObj) {
+		chr.put(chrNum, chrObj);
 	}
 	
-	public String getId() {
-		return id;
-	}
-	
-	public Enumeration<String> getChromosomes () {
-		return chr.keys();
+	public Iterator<String> getChromosomes () {
+		return chr.keySet().iterator();
 	}
 	
 }
